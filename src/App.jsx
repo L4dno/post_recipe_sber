7abb8c6e56a45f8d989bed1bc7d171189b3910c5
L4dno <xmeloff12326@gmail.com>
2024-06-06 17:52:08 +0300

    learned some functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,98 +2,193 @@ import React from 'react';
 import { createAssistant, createSmartappDebugger } from '@salutejs/client';
 
 import './App.css';
+import { TaskList } from './pages/TaskList';
+import {Button} from './components/Button';
 
-import {Info} from './components/Info'
-import {Button} from './components/Button'
-import {Recipe} from './components/Recipe'
+const initializeAssistant = (getState /*: any*/, getRecoveryState) => {
+  if (process.env.NODE_ENV === 'development') {
+    return createSmartappDebugger({
+      token: process.env.REACT_APP_TOKEN ?? '',
+      initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+      getState,                                           
+      // getRecoveryState: getState,                                           
+      nativePanel: {
+        defaultText: 'Новый рецепт',
+        screenshotMode: false,
+        tabIndex: -1,
+    },
+    });
+  } else {
+  return createAssistant({ getState });
+  }
+};
 
 export class App extends React.Component {
   constructor(props) {
-    console.log('constructor');
     super(props);
-
-    // Функция инициализации ассистента
-    this.initialize = (getState, getRecoveryState) => {
-      if (process.env.NODE_ENV === 'development') {
-        // Инициализация ассистента для development среды
-        return createSmartappDebugger({
-          token: process.env.REACT_APP_TOKEN ?? '',
-          initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
-          getState,
-          getRecoveryState: getState,
-          nativePanel: {
-            defaultText: 'Новый рецепт',
-            screenshotMode: false,
-            tabIndex: -1,
-          },
-        });
-      } else {
-        // Инициализация ассистента для production среды
-        return createAssistant({ getState, getRecoveryState });
-      }
-    };
+    console.log('constructor');
 
     this.state = {
-        recipe: undefined, // зарезервированная строка + число
-        button_text: undefined, // подсказка на кнопке
-        rand_number: undefined,
-        error: undefined
-      };
+      number: undefined
+    };
 
-    // Инициализация ассистента
-    this.assistant = this.initialize(() => this.state, () => this.recoveryState);
+    this.assistant = initializeAssistant(() => this.getStateForAssistant());
 
-    // Обработчик события 'data'
-    this.assistant.on('data', (command) => {
-        console.log(`assistant.on(data)`, command);
-      if (command.navigation) {
-        // Обработка навигационных команд ассистента
-        switch (command.navigation.command) {
-          case 'UP':
-            window.scrollTo(0, 0);
-            break;
-          case 'DOWN':
-            window.scrollTo(0, 1000);
-            break;
-          default:
-            break;
-        }
+    this.assistant.on('data', (event /*: any*/) => {
+      console.log(`assistant.on(data)`, event);
+      if (event.type === 'character') {
+        console.log(`assistant.on(data): character: "${event?.character?.id}"`);
+      } else if (event.type === 'insets') {
+        console.log(`assistant.on(data): insets`);
+      } else {
+        const { action } = event;
+        // если приходящий от ассистента ивент отличен от первых двух
+        // то выполняем его отдельно
+        this.dispatchAssistantAction(action);
       }
+    });
+
+    // инициализация при старте приложения
+    this.assistant.on('start', (event) => {
+      let initialData = this.assistant.getInitialData();
+
+      console.log(`assistant.on(start)`, event, initialData);
+    });
+
+    this.assistant.on('command', (event) => {
+      console.log(`assistant.on(command)`, event);
+    });
+
+    this.assistant.on('error', (event) => {
+      console.log(`assistant.on(error)`, event);
+    });
+
+    this.assistant.on('tts', (event) => {
+      console.log(`assistant.on(tts)`, event);
     });
   }
 
-  // Обработчик нажатия кнопки
-  handleOnClick = () => {
-    // Отправка сообщения ассистенту
-    this.assistant.sendData({ action: { action_id: 'gen_number', parameters: { param: 7} } });
-  };
+  // метод вызываемый после монтирования (рендера)
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
 
-  // Обработчик нажатия кнопки обновления
-  handleOnRefreshClick = () => {
-    // Отправка сообщения бэкенду с возможностью подписки на ответ
-    const unsubscribe = this.assistant.sendAction(
-      { action_id: 'some_action_name', parameters: { param: 'some' } },
-      (data) => {
-        // Обработка данных, переданных от бэкенда
-        unsubscribe();
+  // инициализация объекта типа AssistantAppState
+  // хранит данные, которые нужны в бэке для принятия решения
+  getStateForAssistant() {
+    console.log('getStateForAssistant: this.state:', this.state);
+    const state = {
+      // сопоставляет голосовые команды и действия в приложении
+      item_selector: {
+        // 
+        items: this.state.notes.map(({ id, title }, index) => ({
+          number: index + 1,
+          id,
+          title,
+        })),
+        // слова, определенные в сценариях
+        ignored_words: [
+          'добавить','установить','запиши','поставь','закинь','напомнить', // addNote.sc
+          'удалить', 'удали',  // deleteNote.sc
+          'выполни', 'выполнил', 'сделал' // выполнил|сделал
+        ],
       },
-      (error) => {
-        // Обработка ошибки, переданной от бэкенда
+    };
+    console.log('getStateForAssistant: state:', state);
+    return state;
+  }
+
+  dispatchAssistantAction(action) {
+    console.log('dispatchAssistantAction', action);
+    if (action) {
+      switch (action.type) {
+        case 'add_note':
+          return this.add_note(action);
+
+        case 'done_note':
+          return this.done_note(action);
+
+        case 'delete_note':
+          return this.delete_note(action);
+
+        default:
+          throw new Error();
       }
-    );
-  };
+    }
+  }
+
+  add_note(action) {
+    console.log('add_note', action);
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        {
+          id: Math.random().toString(36).substring(7),
+          title: action.note,
+          completed: false,
+        },
+      ],
+    });
+  }
+
+  done_note(action) {
+    console.log('done_note', action);
+    this.setState({
+      notes: this.state.notes.map((note) =>
+        note.id === action.id ? { ...note, completed: !note.completed } : note
+      ),
+    });
+  }
+
+  _send_action_value(action_id, value) {
+    const data = {
+      action: {
+        action_id: action_id,
+        parameters: {
+          // значение поля parameters может быть любым, но должно соответствовать серверной логике
+          value: value, // см.файл src/sc/noteDone.sc смартаппа в Studio Code
+        },
+      },
+    };
+    const unsubscribe = this.assistant.sendData(data, (data) => {
+      // функция, вызываемая, если на sendData() был отправлен ответ
+      const { type, payload } = data;
+      console.log('sendData onData:', type, payload);
+      unsubscribe();
+    });
+  }
+
+  play_done_note(id) {
+    const completed = this.state.notes.find(({ id }) => id)?.completed;
+    if (!completed) {
+      const texts = ['Молодец!', 'Красавчик!', 'Супер!'];
+      const idx = (Math.random() * texts.length) | 0;
+      this._send_action_value('done', texts[idx]);
+    }
+  }
+
+  delete_note(action) {
+    console.log('delete_note', action);
+    this.setState({
+      notes: this.state.notes.filter(({ id }) => id !== action.id),
+    });
+  }
 
   render() {
+    console.log('render');
     return (
-      <div>
-        {/* Кнопка */}
-        <button onClick={this.handleOnClick}>Отправить сообщение ассистенту</button>
-        
-        {/* Кнопка обновления */}
-        <button onClick={this.handleOnRefreshClick}>Отправить сообщение бэкенду</button>
-      </div>
+      <>
+        <TaskList
+          items={this.state.notes}
+          onAdd={(note) => {
+            this.add_note({ type: 'add_note', note });
+          }}
+          onDone={(note) => {
+            this.play_done_note(note.id);
+            this.done_note({ type: 'done_note', id: note.id });
+          }}
+        />
+      </>
     );
   }
 }
-
-export default MyComponent;
